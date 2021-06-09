565205bcdaa719a8fa5b8cf3832e653336102e33
Davis Plude <48025172+slag223@users.noreply.github.com>
2021-06-09 18:22:53 -0400

    comminl;askdhjf

--- KennelShark/settings.py
+++ KennelShark/settings.py
@@ -94,7 +94,7 @@ if DB_URL:
     DEBUG = False
     DATABASES = {
         'default': {
-            'ENGINE': django.db.backends.postgresql_psycopyg2, 
+            'ENGINE': 'django.db.backends.postgresql_psycopyg2', 
             'NAME': DB_NAME,
             'USER': DB_USER,
             'PASSWORD': DB_PASSWORD,
