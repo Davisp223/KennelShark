dad07ad1cbdafa7d3e323e7fc9e26063b390b9fc
Davis Plude <48025172+slag223@users.noreply.github.com>
2021-06-09 14:03:15 -0400

    commmennkasdjfl;aksdjfkl;asjd;fklasj;lasjdfl;kajsdfl;kasjd;fklasjdfl;

--- KennelShark/settings.py
+++ KennelShark/settings.py
@@ -93,7 +93,7 @@ if DB_URL:
     DEBUG = False
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql' 
+            'ENGINE': 'django.db.backends.mysql', 
             'NAME': DB_NAME,
             'USER': DB_USER,
             'PASSWORD': DB_PASSWORD,
