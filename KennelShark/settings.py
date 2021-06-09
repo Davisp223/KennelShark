81c68a3a8e77e1fcce02fb48d03e4c6467f0db55
Davis Plude <48025172+slag223@users.noreply.github.com>
2021-06-09 14:14:52 -0400

    adasasdfawsdfasdfasdf

--- KennelShark/settings.py
+++ KennelShark/settings.py
@@ -82,12 +82,24 @@ WSGI_APPLICATION = 'KennelShark.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+DB_USER = os.getenv("DB_Uname")
+DB_PASSWORD = os.getenv("DB_Pass")
+DB_HOST = os.getenv("DB_Host")
+DB_NAME = os.getenv("DB_Name")
+DB_PORT = os.getenv("DB_Port")
 DB_URL = os.getenv("JAWSDB_URL")
 if DB_URL:
     DEBUG = False
-    import dj_database_url
-    DATABASES = {"default": dj_database_url.config(default=DB_URL)}
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopyg2', 
+            'NAME': DB_NAME,
+            'USER': DB_USER,
+            'PASSWORD': DB_PASSWORD,
+            'HOST': DB_HOST,   # Or an IP Address that your DB is hosted on
+            'PORT': DB_PORT,
+        }
+    }
 else:
     DEBUG = True
     DATABASES = {
