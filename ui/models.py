0b9ffd6d556e86b07e1aa0c45b2faecb428a9871
Davis Plude <48025172+slag223@users.noreply.github.com>
2021-06-14 10:59:45 -0400

    yes'

--- ui/models.py
+++ ui/models.py
@@ -14,6 +14,8 @@ class Entry(models.Model):
         ('K1', 'K1'),
         ('K2', 'K2'),
         ('K3', 'K3'),
+        ('Crates', 'Crates'),
+        ('Rover', 'Rover'),
         ('DayCare', 'DayCare')
     )
     Food_Location = (
